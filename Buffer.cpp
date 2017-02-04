179763c74ca77435fbb987df8f0e91f46f6058ed
Jung <zhanggen.jung@gmail.com>
2017-02-04 17:19:24 +0800

    buffer

--- Buffer.cpp
+++ Buffer.cpp
@@ -1,8 +1,34 @@
 /*********************************************
 * >Author: Jung
 * >Email: jungzhang@xiyoulinux.org
-* >Created Time: 17-2-4
-* >Description: 
+* >Description:
 **********************************************/
 
+#include <sys/uio.h>
 #include "Buffer.h"
+
+const char Buffer::kCRLF[] = "\r\n";
+
+ssize_t Buffer::readFd(int fd, int *saveErrno) {
+    char extrabuf[65536];
+    struct iovec vec[2];
+    const size_t writeable = writeableBytes();
+    vec[0].iov_base = begin() + writerIndex;
+    vec[0].iov_len = writeable;
+    vec[1].iov_base = extrabuf;
+    vec[1].iov_len = sizeof(extrabuf);
+
+    // FIXME:此处应更改为Sockets::readv
+    const ssize_t n = readv(fd, vec, 2);
+
+    if (n < 0) {
+        *saveErrno = errno;
+    } else if (n <= writeable) {
+        writerIndex += n;
+    } else {
+        writerIndex = buffer.size();
+        append(extrabuf, n - writeable);
+    }
+
+    return n;
+}
