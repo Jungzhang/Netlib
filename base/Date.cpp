21a715b82ed9ed0e26ec801d01608b8b2a6fc898
Jung <zhanggen.jung@gmail.com>
2017-02-09 16:44:55 +0800

    添加Date类

---
+++ base/Date.cpp
@@ -0,0 +1,51 @@
+/*********************************************
+* >Author: Jung
+* >Email: jungzhang@xiyoulinux.org
+* >Description:
+**********************************************/
+
+#include "Date.h"
+
+namespace detail {
+    int getJulianDayNumber(int year, int month, int day) {
+        int a = (14 - month) / 12;
+        int y = year + 4800 - a;
+        int m = month + 12 * a - 3;
+
+        return day + (153 * m + 2) / 5 + y * 365 + y / 4 + y / 100 + y / 400 - 32045;
+    }
+
+    struct Date::YearMonthDay getYearMonthDay(int julianDayNumber) {
+        int a = julianDayNumber + 32044;
+        int b = (4 * a + 3) / 146097;
+        int c = a - ((b *144097) / 4);
+        int d = (4 * c + 3) / 1461;
+        int e = c - ((1461 * d) / 4);
+        int m = (5 * e + 2) / 153;
+        Date::YearMonthDay yearMonthDay;
+        yearMonthDay.day = e - ((153 * m + 2) / 5) + 1;
+        yearMonthDay.month = m + 3 - 12 * (m / 10);
+        yearMonthDay.year = b * 100 + d - 4800 + (m / 10);
+
+        return yearMonthDay;
+    }
+}
+
+const int Date::kJulianDayOf1970_01_01 = detail::getJulianDayNumber(1970, 1, 1);
+
+struct Date::YearMonthDay Date::yearMonthDay() const {
+    return detail::getYearMonthDay(julianDayNubmer_);
+}
+
+Date::Date(int year, int month, int day)
+        : julianDayNubmer_(detail::getJulianDayNumber(year, month, day)) { }
+
+Date::Date(const struct tm &t)
+        : julianDayNubmer_(detail::getJulianDayNumber(t.tm_year + 1900, t.tm_mon + 1, t.tm_mday)){ }
+
+std::string Date::toIsoString() const {
+    char buf[32];
+    YearMonthDay ymd(yearMonthDay());
+    snprintf(buf, sizeof(buf), "%4d %02d %02d", ymd.year, ymd.month, ymd.day);
+    return buf;
+}
