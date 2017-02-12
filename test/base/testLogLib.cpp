98403929142aae687d14f82b04add7001a364460
Jung <zhanggen.jung@gmail.com>
2017-02-12 19:09:08 +0800

    使用原子变量重构日志类测试

--- test/base/testLogLib.cpp
+++ test/base/testLogLib.cpp
@@ -9,35 +9,29 @@
 #include <time.h>
 #include <unistd.h>
 #include <cstring>
+#include <atomic>
 #include "../../base/Logger.h"
 
 Netlib::Logger logTest;
 const char *data = "This is test";
-bool isRunig = true;
-std::mutex mutex;
+std::atomic_bool isRunig ;
 
 void testFunc() {
     while (true) {
-        {
-            std::lock_guard<std::mutex> locker(mutex);
-            if (!isRunig) {
-                break;
-            }
+        if (!isRunig) {
+            break;
         }
         logTest.append(data, strlen(data), Netlib::Logger::LogLevel::INFO);
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     logTest.setLogFileInfo("Test");
+    isRunig = true;
     std::thread t1(testFunc);
 
-    std::this_thread::sleep_for(std::chrono::seconds(60));
-    {
-        std::lock_guard<std::mutex> locker(mutex);
-        isRunig = false;
-    }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    isRunig = false;
     t1.join();
 
     return EXIT_SUCCESS;
