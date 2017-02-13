a1ab4e724de19ceb2d4188781ae9832e3e864caf
Jung <zhanggen.jung@gmail.com>
2017-02-13 20:04:51 +0800

    创建封装IO多路复用的Poller类

---
+++ net/Poller.cpp
@@ -0,0 +1,7 @@
+/*********************************************
+* >Author: Jung
+* >Email: jungzhang@xiyoulinux.org
+* >Description:
+**********************************************/
+
+#include "Poller.h"
---
+++ net/Poller.h
@@ -0,0 +1,16 @@
+/*********************************************
+* >Author: Jung
+* >Email: jungzhang@xiyoulinux.org
+* >Description:
+**********************************************/
+
+#ifndef NETLIB_POLLER_H
+#define NETLIB_POLLER_H
+
+
+class Poller {
+
+};
+
+
+#endif //NETLIB_POLLER_H
