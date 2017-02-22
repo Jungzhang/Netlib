b8a56f42482f57a0cae392da59be9ac66fc6e024
Jung <zhanggen.jung@gmail.com>
2017-02-22 11:31:56 +0800

    添加多线程Reactor的测试

---
+++ test/net/testMuiltThreadServer.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: testMuiltThreadServer.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org
+	> Description:
+ ************************************************************************/
+#include "../../net/Callbacks.h"
+#include "../../net/TcpConnection.h"
+#include "../../net/EventLoop.h"
+#include "../../net/TcpServer.h"
+
+void onConnection(const Netlib::TcpConnectionPtr &conn) {
+    if (conn->connected()) {
+        printf("一个新连接，name: %s  IP：%s\n", conn->name().c_str()
+                , conn->peerAddress().toHostPort().c_str());
+    } else {
+        printf("一个连接离开了，name: %s\n", conn->name().c_str());
+    }
+}
+
+void onMessage(const Netlib::TcpConnectionPtr &conn, Netlib::Buffer *buf, Netlib::TimeStamp re) {
+    std::string str(std::move(buf->retrieveAllAsString()));
+    printf("收到来自 %s 的消息：%s\n", conn->peerAddress().toHostPort().c_str()
+            , str.c_str());
+    conn->send(str);
+}
+
+int main() {
+
+    Netlib::InetAddress inetAddress(9001);
+
+    Netlib::EventLoop eventLoop;
+    Netlib::TcpServer tcpServer(&eventLoop, inetAddress);
+    tcpServer.setConnectionCallback(onConnection);
+    tcpServer.setMessageCallback(onMessage);
+    tcpServer.setThreadNum(4);
+    tcpServer.start();
+    eventLoop.loop();
+
+    return 0;
+}
+
