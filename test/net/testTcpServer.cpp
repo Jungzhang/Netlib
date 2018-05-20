a451ca1f704dd0f1ada6eb0d3c3c19cfbbe3744b
Jung <zhanggen.Jung@gmail.com>
2018-05-20 20:33:04 +0800

    Update testTcpServer.cpp

--- test/net/testTcpServer.cpp
+++ test/net/testTcpServer.cpp
@@ -20,26 +20,18 @@ void onConnection(const Netlib::TcpConnectionPtr &conn) {
         printf("一个连接离开了, name:%s\n", conn->name().c_str());
     }
 }
-
 void onMessage(const Netlib::TcpConnectionPtr &conn, Netlib::Buffer *buf, Netlib::TimeStamp re) {
     printf("received %zd bytes from connection : %s\n", buf->readableBytes()
             , conn->peerAddress().toHostPort().c_str());
     printf("the data : %s\n", buf->retrieveAllAsString().c_str());
 }
-
-
 int main() {
-
-    printf("main() : pid = %d\n", getpid());
     Netlib::InetAddress listenAddr(9001);
     Netlib::EventLoop loop;
-
     Netlib::TcpServer server(&loop, listenAddr);
     server.setConnectionCallback(onConnection);
     server.setMessageCallback(onMessage);
     server.start();
-
     loop.loop();
-
     return 0;
 }
